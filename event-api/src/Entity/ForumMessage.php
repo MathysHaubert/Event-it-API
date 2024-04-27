e75216e76939c413d91c4626e19b93bf88b49d74
Mathys Haubert <mathys.haubert80@gmail.com>
2024-04-27 13:31:59 +0200

    fix "target entity cannot be found" when creating database

--- event-api/src/Entity/ForumMessage.php
+++ event-api/src/Entity/ForumMessage.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
-#[ORM\Table(name: "Forum_message")]
+#[ORM\Table(name: "ForumMessage")]
 class ForumMessage
 {
     #[ORM\Id]
